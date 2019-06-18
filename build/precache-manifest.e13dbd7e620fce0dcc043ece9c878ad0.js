2ed9d16e666d0e56bb3a2e58291ecc7f4144ab07
Garrick Ngai <ngai.garrick.j@gmail.com>
2019-06-17 23:03:56 -0700

    created build and optimized app

---
+++ build/precache-manifest.e13dbd7e620fce0dcc043ece9c878ad0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ad3204202c337a9970a",
+    "url": "/static/js/main.7ad32042.chunk.js"
+  },
+  {
+    "revision": "a7a7a04a228e4c909684",
+    "url": "/static/js/1.a7a7a04a.chunk.js"
+  },
+  {
+    "revision": "7ad3204202c337a9970a",
+    "url": "/static/css/main.493bd0a3.chunk.css"
+  },
+  {
+    "revision": "a7a7a04a228e4c909684",
+    "url": "/static/css/1.09051171.chunk.css"
+  },
+  {
+    "revision": "837f15934718511f96a2f0958817c4ee",
+    "url": "/index.html"
+  }
+];
