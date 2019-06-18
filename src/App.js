d77d131174c1dc331dd8366dffe7b9929547bdc7
Garrick Ngai <ngai.garrick.j@gmail.com>
2019-06-17 22:36:23 -0700

    created json request for users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import CardList from './CardList';
 import SearchBox from './SearchBox'
-import { robots } from './robots'
 import './App.css'
 
 
@@ -9,9 +8,16 @@ class App extends Component {
   constructor(){
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield:''
     }
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users})
+    );
   }
 
   onSearchChange = (event) => {
@@ -26,7 +32,7 @@ class App extends Component {
 
     return (
       <div className='tc'>
-        <h1>RoboFriends</h1>
+        <h1 className='f1'>RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
         <CardList robots={filteredRobots}/>
       </div>
